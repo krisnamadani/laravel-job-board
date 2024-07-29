dd8544a84cbd1985bb92b09f49022fa18dd8f7ab
krisna <krisnamadani@gmail.com>
2024-07-29 20:57:18 +0700

    tag component & job info

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,7 +1,29 @@
 <x-layout>
   @foreach ($posts as $post)
     <x-card class="mb-4">
-      {{ $post->title }}
+      <div class="mb-4 flex justify-between">
+        <h2 class="text-lg font-medium">{{ $post->title }}</h2>
+        <div class="text-slate-500">
+          ${{ number_format($post->salary) }}
+        </div>
+      </div>
+
+      <div class="mb-4 flex justify-between text-sm text-slate-500">
+        <div class="flex space-x-4">
+          <div>Company Name</div>
+          <div>{{ $post->location }}</div>
+        </div>
+        <div class="flex space-x-1 text-xs">
+          <x-tag>
+            {{ Str::ucfirst($post->experience) }}
+          </x-tag>
+          <x-tag>{{ $post->category }}</x-tag>
+        </div>
+      </div>
+
+      <p class="text-sm text-slate-500">
+        {!! nl2br(e($post->description)) !!}
+      </p>
     </x-card>
   @endforeach
 </x-layout>
