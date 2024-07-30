2fff2515646853e643f7ef9a010333738a9825b4
krisna <krisnamadani@gmail.com>
2024-07-30 14:37:19 +0700

    radio button filters

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -18,11 +18,13 @@ class PostController extends Controller
             $query->where(function ($query) {
                 $query->where('title', 'like', '%' . request('search') . '%')
                     ->orWhere('description', 'like', '%' . request('search') . '%');
-                });
-            })->when(request('min_salary'), function ($query) {
-                $query->where('salary', '>=', request('min_salary'));
-            })->when(request('max_salary'), function ($query) {
-                $query->where('salary', '<=', request('max_salary'));
+            });
+        })->when(request('min_salary'), function ($query) {
+            $query->where('salary', '>=', request('min_salary'));
+        })->when(request('max_salary'), function ($query) {
+            $query->where('salary', '<=', request('max_salary'));
+        })->when(request('experience'), function ($query) {
+            $query->where('experience', request('experience'));
         });
 
         return view('post.index', ['posts' => $posts->get()]);
