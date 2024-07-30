a0f20518d62929c7ccbae520590194761353d02c
krisna <krisnamadani@gmail.com>
2024-07-30 14:20:48 +0700

    tailwind form plugin & text inputs

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,5 +1,24 @@
 <x-layout>
   <x-breadcrumbs class="mb-4" :links="['Posts' => route('posts.index')]" />
+
+  <x-card class="mb-4 text-sm">
+    <div class="mb-4 grid grid-cols-2 gap-4">
+      <div>
+        <div class="mb-1 font-semibold">Search</div>
+        <x-text-input name="search" value="" placeholder="Search for any text" />
+      </div>
+      <div>
+        <div class="mb-1 font-semibold">Salary</div>
+
+        <div class="flex space-x-2">
+          <x-text-input name="min_salary" value="" placeholder="From" />
+          <x-text-input name="max_salary" value="" placeholder="To" />
+        </div>
+      </div>
+      <div>3</div>
+      <div>4</div>
+    </div>
+  </x-card>
   
   @foreach ($posts as $post)
     <x-post-card class="mb-4" :$post>
