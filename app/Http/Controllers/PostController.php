b5138d5cf4621becdeb09634efba67e162dee24c
krisna <krisnamadani@gmail.com>
2024-08-01 22:21:58 +0700

    other employer jobs

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -38,7 +38,7 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        return view('post.show', ['post' => $post->load('employer')]);
+        return view('post.show', ['post' => $post->load('employer.posts')]);
     }
 
     /**
--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -7,4 +7,30 @@
       {!! nl2br(e($post->description)) !!}
     </p>
   </x-post-card>
+
+  <x-card class="mb-4">
+    <h2 class="mb-4 text-lg font-medium">
+      More {{ $post->employer->company_name }} Posts
+    </h2>
+
+    <div class="text-sm text-slate-500">
+      @foreach ($post->employer->posts as $otherPost)
+        <div class="mb-4 flex justify-between">
+          <div>
+            <div class="text-slate-700">
+              <a href="{{ route('posts.show', $otherPost) }}">
+                {{ $otherPost->title }}
+              </a>
+            </div>
+            <div class="text-xs">
+              {{ $otherPost->created_at->diffForHumans() }}
+            </div>
+          </div>
+          <div class="text-xs">
+            ${{ number_format($otherPost->salary) }}
+          </div>
+        </div>
+      @endforeach
+    </div>
+  </x-card>
 </x-layout>
