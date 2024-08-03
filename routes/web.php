2d913b9e95cceea0c8c9a90393d82357be608500
krisna <krisnamadani@gmail.com>
2024-08-03 16:46:34 +0700

    auth controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
-use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
 
-Route::resource('posts', PostController::class)->only(['index', 'show']);
+Route::resource('posts', PostController::class)
+  ->only(['index', 'show']);
+
+Route::get('login', fn() => to_route('auth.create'))->name('login');
+
+Route::resource('auth', AuthController::class)
+  ->only(['create', 'store']);
